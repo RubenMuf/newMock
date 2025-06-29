d266e78c5f18caf88daf2f63af0789e325d189cb
RubenMuf <rd.promiwka@yandex.ru>
2025-06-29 23:07:46 +0300

    Comm2

--- src/main/java/com/example/newMock/model/RequestDTO.java
+++ src/main/java/com/example/newMock/model/RequestDTO.java
@@ -12,20 +12,13 @@ public class RequestDTO {
     private String rqUID;
     private String clientId;
     private String account;
-    private String currency;
-    private String balance;
-    private String maxLimit;
+    private String openDate;
+    private String closeDate;
 
 }
 
-//        "rqUID": "79dgtf565j8158f64gt4",
-//        "clientId": "8050000000000000000",
-//        "account": "80500000000000000001",
-//        "currency": "US",
-//        "balance": "1400.00",
-//        "maxLimit": "2000.00"
 
-//"rqUID": "58dgtf565j8547f64ke7",
+//        "rqUID": "58dgtf565j8547f64ke7",
 //        "clientId": "3050000000000000000",
 //        "account": "30500000000000000001",
 //        "openDate": "2020-01-01",
